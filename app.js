f0d248af59b7f05627b50436a4072eb9867f494b
Brian Bud <brian@brianbud.com>
2022-06-13 20:19:12 -0400

    boolean to stop players if any player hit winning score

--- app.js
+++ app.js
@@ -6,17 +6,24 @@ const p2Display = document.querySelector('#player2Display');
 let p1Score = 0;
 let p2Score = 0;
 let winningScore = 5;
+let isGameOver = false;
 
 player1Btn.addEventListener('click', function () {
-  if (p1Score !== winningScore) {
+  if (!isGameOver) {
     p1Score++;
+    if (p1Score === winningScore) {
+      isGameOver = true;
+    }
     p1Display.textContent = p1Score;
   }
 });
 
 player2Btn.addEventListener('click', function () {
-  if (p2Score !== winningScore) {
+  if (!isGameOver) {
     p2Score++;
+    if (p2Score === winningScore) {
+      isGameOver = true;
+    }
     p2Display.textContent = p2Score;
   }
 });
