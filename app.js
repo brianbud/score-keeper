af182891247a00420e5a54fba35f11fac631646c
Brian Bud <brian@brianbud.com>
2022-06-13 20:11:49 -0400

    player 2 increment score

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ const p1Display = document.querySelector('#player1Display');
 const p2Display = document.querySelector('#player2Display');
 
 let p1Score = 0;
+let p2Score = 0;
 let winningScore = 5;
 
 player1Btn.addEventListener('click', function () {
   if (p1Score !== winningScore) {
     p1Score++;
     p1Display.textContent = p1Score;
+  }
+});
+
+player2Btn.addEventListener('click', function () {
+  if (p2Score !== winningScore) {
+    p2Score++;
+    p2Display.textContent = p2Score;
   }
 });
